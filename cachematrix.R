d9ddf44a9f400690a442ec8b57bf2b8562d338e4
sunitv99 <46061197+sunitv99@users.noreply.github.com>
2020-10-18 17:24:57 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -30,3 +30,14 @@ cacheSolve <- function(x, ...) {
 	x$setInv(mat)
 	mat	
 }
+
+
+##Testing conditions
+##a1 <- c(3,2,5)
+##a2 <- c(2,3,2)
+##a3 <- c(5,2,4)
+##A <- rbind(a1,a2,a3)
+##mm <- makeCacheMatrix(A)
+##mm$get()
+##cacheSolve(mm)
+##mm$getInv()
